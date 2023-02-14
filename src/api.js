7fe9062b07a3ee97c95f0fb3adb38a97c013eb95
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2023-02-14 18:51:21 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd7e41d81eemsh30e8ea3b239f7d4p15b4cbjsnd292938f88fa',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
